daf495d05a8eb668ca39b7ccd6dc7336e5a2f570
suhyeon <suhyeon3484@naver.com>
2023-08-27 00:03:14 +0900

    feat: CORS 설정

--- pages/api/baseFetch.ts
+++ pages/api/baseFetch.ts
@@ -1,3 +1,18 @@
 export const fetchWithBaseURL = (url: string, options?: RequestInit) => {
-  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, options);
+  const corsHeaders = {
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Accept',
+  };
+
+  const combinedOptions = {
+    ...options,
+    headers: {
+      ...options?.headers,
+      ...corsHeaders,
+    },
+  };
+
+  return fetch(`${process.env.NEXT_PUBLIC_BASE_URL}${url}`, combinedOptions);
 };
